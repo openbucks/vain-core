692de8f69802ec7e491223801139ca71ae13dda1
Taras Girnyk <le@mirohost.net>
2016-04-04 20:34:00 +0300

    Renamed files

--- src/Exception/CoreException.php
+++ src/Exception/CoreException.php
@@ -8,8 +8,14 @@
 
 namespace Vain\Core\Exception;
 
-class VainCoreException extends \Exception
+class CoreException extends \Exception
 {
+    /**
+     * CoreException constructor.
+     * @param string $message
+     * @param int $code
+     * @param \Exception|null $previous
+     */
     public function __construct($message, $code, \Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
