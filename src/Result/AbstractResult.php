59bdb4b046442bc7880c4f80fe17ae4ddf272fbf
allflame <taras.p.gyrnik@gmail.com>
2016-05-26 11:30:18 +0300

    Added abstractresultclass

---
+++ src/Result/AbstractResult.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: allflame
+ * Date: 5/26/16
+ * Time: 11:29 AM
+ */
+
+namespace Vain\Core\Result;
+
+abstract class AbstractResult implements ResultInterface
+{
+    private $status;
+
+    /**
+     * AbstractResult constructor.
+     * @param bool $status
+     */
+    public function __construct($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+}
---
+++ src/Result/ResultInterface.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: allflame
+ * Date: 5/26/16
+ * Time: 11:28 AM
+ */
+
+namespace Vain\Core\Result;
+
+
+interface ResultInterface
+{
+    /**
+     * @return bool
+     */
+    public function getStatus();
+}
