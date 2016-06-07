5ae6336acf75cb0de09bbc8ebc0d19c0ee6f8af4
allflame <taras.p.gyrnik@gmail.com>
2016-06-07 09:50:54 +0300

    Added StringInterface

---
+++ src/String/StringInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: allflame
+ * Date: 6/7/16
+ * Time: 9:50 AM
+ */
+
+namespace Vain\Core\String;
+
+interface StringInterface
+{
+    /**
+     * @return string
+     */
+    public function __toString();
+}
