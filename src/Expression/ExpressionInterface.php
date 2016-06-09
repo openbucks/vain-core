6d698779a8fca92f2390c5a8b7651ddaa22bb6f1
allflame <taras.p.gyrnik@gmail.com>
2016-06-09 09:16:33 +0300

    Switched tr

---
+++ src/Expression/ExpressionInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: allflame
+ * Date: 6/8/16
+ * Time: 9:46 AM
+ */
+
+namespace Vain\Core\Expression;
+
+use Vain\Core\String\StringInterface;
+
+interface ExpressionInterface extends StringInterface
+{
+
+}
