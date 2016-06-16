fb842c0c514070f3ebd0e12f6fa40d6fa1220f1f
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-06-16 09:34:08 +0300

    Added ArrayInterface

---
+++ src/ArrayX/ArrayInterface.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: allflame
+ * Date: 6/16/16
+ * Time: 9:33 AM
+ */
+
+namespace Vain\Core\ArrayX;
+
+interface ArrayInterface
+{
+    /**
+     * @return array
+     */
+    public function toArray();
+}
