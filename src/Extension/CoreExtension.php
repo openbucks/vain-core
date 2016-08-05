0d71184a215d6e312b13667176653e8f0ae61415
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-08-05 12:14:30 +0300

    Added Extension

---
+++ src/Extension/CoreExtension.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-core
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-core
+ */
+namespace Vain\Core\Extension;
+
+/**
+ * Class CoreExtension
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class CoreExtension extends AbstractExtension
+{
+}
