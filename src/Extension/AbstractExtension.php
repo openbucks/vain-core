570fe564c6e786905c35fcd07c3add10ada9e60f
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-08-05 12:11:08 +0300

    Added Extension

---
+++ src/Extension/AbstractExtension.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-core
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-core
+ */
+namespace Vain\Core\Extension;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+/**
+ * Class AbstractExtension
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+abstract class AbstractExtension extends Extension
+{
+    /**
+     * @inheritDoc
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(
+                sprintf(
+                    '%s%s%s%s%s',
+                    dirname((new \ReflectionClass(get_class($this)))->getFileName()),
+                    DIRECTORY_SEPARATOR,
+                    '..',
+                    DIRECTORY_SEPARATOR,
+                    $container->getParameter('app.config.dir')
+                )
+            )
+        );
+        $loader->load('di.yml');
+    }
+}
