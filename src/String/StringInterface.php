c1fa68373bc1f9ed34e647614afd4df6bf139413
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-09-19 11:13:26 +0300

    Code style fixes

--- src/String/StringInterface.php
+++ src/String/StringInterface.php
@@ -8,7 +8,7 @@
  * @license   https://opensource.org/licenses/MIT MIT License
  * @link      https://github.com/allflame/vain-core
  */
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace Vain\Core\String;
 
