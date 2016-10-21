aff6e392a0d51a585b72a04507cb3dabc8d3ec1c
Taras Girnyk <taras.p.gyrnik@gmail.com>
2016-10-21 09:24:48 +0300

    Added container interface

---
+++ src/Container/ContainerInterface.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-core
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-core
+ */
+declare(strict_types = 1);
+
+namespace Vain\Core\Container;
+
+/**
+ * Interface ContainerInterface
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+interface ContainerInterface
+{
+    /**
+     * @param string $serviceId
+     *
+     * @return mixed
+     */
+    public function getService(string $serviceId);
+}
