9ef63fbde61babf1a58b0e1bcdcd52d326fac532
allflame <taras.p.gyrnik@gmail.com>
2016-12-12 19:00:02 +0200

    PhpCs compliance

---
+++ src/Display/DisplayableInterface.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-core
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-core
+ */
+declare(strict_types = 1);
+
+namespace Vain\Core\Display;
+
+/**
+ * Class DisplayableInterface
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+interface DisplayableInterface
+{
+    /**
+     * @return array
+     */
+    public function toDisplay() : array;
+}
