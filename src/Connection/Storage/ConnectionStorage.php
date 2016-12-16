35bd0dec9d4d3b75904b8d46c6231c6429da8479
allflame <taras.p.gyrnik@gmail.com>
2016-12-17 01:11:34 +0200

    Refactored connection storage

--- src/Connection/Storage/ConnectionStorage.php
+++ src/Connection/Storage/ConnectionStorage.php
@@ -12,19 +12,22 @@ declare(strict_types = 1);
 
 namespace Vain\Core\Connection\Storage;
 
+use Vain\Core\Connection\ConnectionInterface;
 use Vain\Core\Name\Storage\AbstractNameableStorage;
 
 /**
  * Class ConnectionStorage
  *
  * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ *
+ * @method ConnectionInterface getItem(string $connectionName)
  */
 class ConnectionStorage extends AbstractNameableStorage implements ConnectionStorageInterface
 {
     /**
      * @inheritDoc
      */
-    public function getConnection(string $connectionName)
+    public function getConnection(string $connectionName) : ConnectionInterface
     {
         return $this->getItem($connectionName);
     }
