e0c2126f5a4c05bc0265a5a8ea0c3691ad6bbec6
allflame <taras.p.gyrnik@gmail.com>
2016-12-16 07:27:43 +0200

    Moved connection to the core

---
+++ src/Api/Extension/AbstractApiExtension.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-api
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-api
+ */
+declare(strict_types = 1);
+
+namespace Vain\Core\Api\Extension;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+/**
+ * Class AbstractApiExtension
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+abstract class AbstractApiExtension extends Extension
+{
+    /**
+     * @inheritDoc
+     */
+    public function load(array $configs, ContainerBuilder $container) : AbstractApiExtension
+    {
+        $absolutePath = sprintf(
+            '%s%s..%s',
+            dirname((new \ReflectionClass(get_class($this)))->getFileName()),
+            DIRECTORY_SEPARATOR,
+            DIRECTORY_SEPARATOR
+        );
+
+        $loader = new YamlFileLoader($container, new FileLocator($absolutePath));
+        $loader->load('di.yml');
+
+        if (false === $container->has('api.config.composite') || false === $container->hasParameter('app.dir')) {
+            return $this;
+        }
+
+        $appDir = $container->getParameter('app.dir');
+        if (false === strpos($absolutePath, $appDir)) {
+            return $this;
+        }
+
+        $container
+            ->findDefinition('api.config.composite')
+            ->addMethodCall(
+                'addFile',
+                [substr($absolutePath, strlen($appDir) + 1) . 'api']
+            );
+
+        return $this;
+    }
+}
