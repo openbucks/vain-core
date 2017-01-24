608295fd70d000739f648a240954702811aa5658
allflame <taras.p.gyrnik@gmail.com>
2017-01-24 23:19:34 +0200

    Added Phinx, PostgresAdapter

---
+++ src/Database/Phinx/PhinxPostgresAdapter.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-core
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-core
+ */
+declare(strict_types = 1);
+
+namespace Vain\Core\Database\Phinx;
+
+use Phinx\Db\Adapter\PostgresAdapter;
+
+/**
+ * Class PhinxPostgresAdapter
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class PhinxPostgresAdapter extends PostgresAdapter
+{
+    const PHINX_TYPE_INET = 'inet';
+
+    /**
+     * @inheritDoc
+     */
+    public function getColumnTypes()
+    {
+        return array_merge(parent::getColumnTypes(), ['inet']);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getSqlType($type, $limit = null)
+    {
+        if (static::PHINX_TYPE_INET === $type) {
+            return ['name' => $type];
+        }
+
+        return parent::getSqlType($type, $limit);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getPhinxType($sqlType)
+    {
+        if ('inet' === $sqlType) {
+            return static::PHINX_TYPE_INET;
+        }
+
+        return parent::getPhinxType($sqlType);
+    }
+}
