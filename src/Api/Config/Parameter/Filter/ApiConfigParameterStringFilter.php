4132cabaa83d88865315def1d85f975b678a75ec
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-04-04 11:48:06 +0300

    Return null on empty string

--- src/Api/Config/Parameter/Filter/ApiConfigParameterStringFilter.php
+++ src/Api/Config/Parameter/Filter/ApiConfigParameterStringFilter.php
@@ -8,7 +8,7 @@
  * @license   https://opensource.org/licenses/MIT MIT License
  * @link      https://github.com/allflame/vain-api
  */
-declare(strict_types = 1);
+declare(strict_types=1);
 
 namespace Vain\Core\Api\Config\Parameter\Filter;
 
@@ -28,7 +28,12 @@ class ApiConfigParameterStringFilter extends AbstractApiConfigParameterFilter
      */
     public function doFilter(string $name, $element): ApiConfigParameterResultInterface
     {
-        if (false === ($string = filter_var($element, FILTER_SANITIZE_STRING))) {
+        if (false === ($string = filter_var(
+                $element,
+                FILTER_SANITIZE_STRING,
+                ['flags' => FILTER_FLAG_EMPTY_STRING_NULL]
+            ))
+        ) {
             return new ApiParameterWrongTypeResult($name, 'string', $element);
         }
 
