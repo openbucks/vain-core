2a2c184751199079b53804d9fad714bb3f3a948d
allflame <taras.p.gyrnik@gmail.com>
2017-05-08 17:06:46 +0300

    Added entity, document storages

---
+++ src/Api/Extension/Storage/ApiExtensionDocumentStorage.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Vain Framework
+ *
+ * PHP Version 7
+ *
+ * @package   vain-core
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://github.com/allflame/vain-core
+ */
+declare(strict_types = 1);
+
+namespace Vain\Core\Api\Extension\Storage;
+
+/**
+ * Class ApiExtensionDocumentStorage
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+class ApiExtensionDocumentStorage implements ApiExtensionStorageInterface
+{
+    private $paths = [];
+
+    /**
+     * @inheritDoc
+     */
+    public function addPath(string $path, string $namespace) : ApiExtensionStorageInterface
+    {
+        $this->paths[$path] = $namespace;
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getPaths() : array
+    {
+        return $this->paths;
+    }
+}
